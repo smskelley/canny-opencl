f87bcf9f8b083c37d68c619def0f897a781606e3
Jared M. Olson <jaredmolson84@gmail.com>
2014-11-02 15:30:05 -0500

    Removed debugging code from canny-opencl.cpp main function.

--- src/canny-opencl.cpp
+++ src/canny-opencl.cpp
@@ -15,7 +15,6 @@ int main(int argc, char *argv[])
 {
     cv::VideoCapture webcam(0);
     cv::Mat inFrame, grayFrame;
-    cv::Mat angle_matrix;
     bool useGPU = true;
     if (argc > 1 && strcmp(argv[1], "cpu") == 0)
         useGPU = false;
@@ -35,9 +34,6 @@ int main(int argc, char *argv[])
             processor.FinishJobs();
         }
     
-        angle_matrix = processor.GetTheta();
-        cout << endl << endl << angle_matrix << endl << endl;        
-
         imshow("canny", processor.GetOutput());
         if (cv::waitKey(30) >= 0)
             break;
