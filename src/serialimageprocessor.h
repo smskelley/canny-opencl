28e4f00b715f1d4cd464e68813b7f31fc9acc4bb
Jared M. Olson <jaredmolson84@gmail.com>
2014-11-24 13:36:01 -0500

    Added serial versions of the three kernels. This is currently not compiling due to issues with the way we are addressing the Mat data structures. Mat does not natively support using input[pos] or input[row][col].

--- src/serialimageprocessor.h
+++ src/serialimageprocessor.h
@@ -9,8 +9,8 @@ class SerialImageProcessor : public ImageProcessor
 
     // These methods are blocking calls which will perform what their name
     // implies
-    virtual void Gaussian();
-    virtual void Sobel();
-    virtual void NonMaxSuppression();
+    virtual void Gaussian(cv::Mat data, cv::Mat out);
+    virtual void Sobel(cv::Mat data, cv::Mat out, cv::Mat theta);
+    virtual void NonMaxSuppression(cv::Mat data, cv::Mat out, cv::Mat theta);
     virtual void HysteresisThresholding();
 };
