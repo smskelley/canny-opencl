1aa4c04c0a0e668dd2c48f11a033869312093dad
Sean Kelley <smskelley@gmail.com>
2014-12-03 13:17:43 -0500

    Add very large and very small images to benchmark.

--- src/benchmark-suite.cpp
+++ src/benchmark-suite.cpp
@@ -17,7 +17,9 @@ int main(int argc, char *argv[])
 {
     vector<Benchmark> benchmarks;
     vector<InputImage> input_images {
+        InputImage("world.jpg", 24000, 12000),
         InputImage("Great_Tit.jpg", 2948, 2057),
+        InputImage("lena.jpg", 100, 100),
         InputImage("hs-2004-07-a-full_jpg.jpg", 6200, 6200)
     };
 
@@ -26,25 +28,25 @@ int main(int argc, char *argv[])
     for (auto image : input_images)
     {
         // OpenCL GPU & CPU benchmarks
-        benchmarks.push_back(
-            Benchmark("OpenCL GPU",
-            shared_ptr<ImageProcessor>(new OpenclImageProcessor(true)),
-            IMG_PATH, image, 3));
+//        benchmarks.push_back(
+//            Benchmark("OpenCL GPU",
+//            shared_ptr<ImageProcessor>(new OpenclImageProcessor(true)),
+//            IMG_PATH, image, 3));
 
         benchmarks.push_back(
             Benchmark("OpenCL CPU",
             shared_ptr<ImageProcessor>(new OpenclImageProcessor(false)),
             IMG_PATH, image, 3));
 
-        benchmarks.push_back(
-            Benchmark("Serial",
-            shared_ptr<ImageProcessor>(new SerialImageProcessor()),
-            IMG_PATH, image, 3));
-
-        benchmarks.push_back(
-            Benchmark("OpenCV",
-            shared_ptr<ImageProcessor>(new CvImageProcessor()),
-            IMG_PATH, image, 3));
+//        benchmarks.push_back(
+//            Benchmark("Serial",
+//            shared_ptr<ImageProcessor>(new SerialImageProcessor()),
+//            IMG_PATH, image, 3));
+//
+//        benchmarks.push_back(
+//            Benchmark("OpenCV",
+//            shared_ptr<ImageProcessor>(new CvImageProcessor()),
+//            IMG_PATH, image, 3));
 
     }
     
